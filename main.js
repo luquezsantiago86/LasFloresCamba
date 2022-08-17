2b7118877a26e4b739f15b3aedfc1b1c2034beea
luquezsantiago86 <luquezsantiago86@gmail.com>
2022-08-17 15:50:16 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+$('#devBtn').on('click', function(e) {
+    e.preventDefault();
+    $('#devModal').modal('show').find('.modal-content').load($(this).attr('href'));
+
+});
+
+$('#devContact').on('click', function(e) {
+    e.preventDefault();
+    $('#devContact').find('.modal-content').load($(this).attr('href'));
+
+});
+
+$('#dashboardingBtn').on('click', function(e) {
+    e.preventDefault();
+    $('#devModal').modal('show').find('.modal-content').load($(this).attr('href'));
+
+});
+
+$('#gprTrackerBtn').on('click', function(e) {
+    e.preventDefault();
+    $('#devModal').modal('show').find('.modal-content').load($(this).attr('href'));
+
+});
+
+$('#tomographyBtn').on('click', function(e) {
+    e.preventDefault();
+    $('#devModal').modal('show').find('.modal-content').load($(this).attr('href'));
+
+});
+
